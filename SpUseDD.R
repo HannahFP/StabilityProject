862d6e485f38d114fedac2c0849e0697c181b909
HannahFP <hannah.fried.petersen@slu.se>
2017-09-11 16:28:24 +0200

    Delete SpUseDD.R

--- SpUseDD.R
+++
@@ -1,3 +0,0 @@
-DDBMI <- read.csv("file:///C:/Users/hhen0001/Documents/Stability_BMI_56Lakes/Data Files/OriginalDatafromDavid_CSV.csv", header=TRUE, sep=",")
-SpUse <- colnames(DDBMI[7:length(DDBMI)])
-SpUse <- gsub("\\.", " ", SpUse)
